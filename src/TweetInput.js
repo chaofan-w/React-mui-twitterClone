eb9e149be72ba9acb2b3a334a761cff091ba59a2
chaofan-w <chaofan.w@gmail.com>
2022-12-12 21:13:54 -0500

    tweet editing field interation features

--- src/TweetInput.js
+++ src/TweetInput.js
@@ -42,8 +42,66 @@ import {
   SupervisedUserCircle,
   KeyboardArrowDown,
   Check,
+  PhotoOutlined,
+  GifBoxOutlined,
+  BallotOutlined,
+  SentimentSatisfiedOutlined,
+  CalendarMonthOutlined,
+  LocationOnOutlined,
 } from "@mui/icons-material";
 import styled from "styled-components";
+import ReplyAudienceSelectPopover from "./ReplyAudienceSelectPopover copy";
+
+const tweetFeatureIcons = [
+  {
+    icon: (
+      <PhotoOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertPhoto",
+  },
+  {
+    icon: (
+      <GifBoxOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertGif",
+  },
+  {
+    icon: (
+      <BallotOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertPoll",
+  },
+  {
+    icon: (
+      <SentimentSatisfiedOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertEmoji",
+  },
+  {
+    icon: (
+      <CalendarMonthOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertSchedule",
+  },
+  {
+    icon: (
+      <LocationOnOutlined
+        sx={{ width: "20px", height: "20px", color: "primary.main" }}
+      />
+    ),
+    label: "insertLocation",
+  },
+];
 
 const TweetInput = () => {
   const [value, setValue] = React.useState("");
@@ -73,10 +131,13 @@ const TweetInput = () => {
         multiline
         rowsMax="3"
         sx={{
+          // border: "1px solid green",
           fontSize: "20px",
-          maxWidth: "100%",
+          width: "100%",
           color: "transparent",
+          caretColor: "black",
           zIndex: 10,
+          mb: 3,
           "& textarea::placeholder": {
             color: "grey",
           },
@@ -86,20 +147,75 @@ const TweetInput = () => {
         <Typography
           sx={{
             width: "100%",
-            maxWidth: 480,
             overflowWrap: "break-word",
             fontSize: "20px",
             position: "absolute",
-            border: "1px solid black",
-            top: 3,
+            // border: "1px solid black",
+            top: 0,
+            left: 0,
+            mb: 3,
           }}
         >
           <span>{value.length <= 140 ? value : value.slice(0, 140)}</span>
-          <span style={{ color: "red" }}>
+          <span style={{ backgroundColor: "lightcoral" }}>
             {value.length > 140 ? value.slice(140) : null}
           </span>
         </Typography>
       )}
+      <Box
+        sx={{
+          mt: 3,
+          mb: 3,
+        }}
+      >
+        <ReplyAudienceSelectPopover />
+      </Box>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{
+          borderTop: "1px solid rgb(225,225,225) ",
+          pt: 2,
+          pb: 2,
+        }}
+      >
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-start"
+          sx={{ width: "50%", p: 0, m: 0 }}
+        >
+          {tweetFeatureIcons.map((icon) => (
+            <IconButton
+              sx={{
+                width: "30px",
+                height: "30px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {icon.icon}
+            </IconButton>
+          ))}
+        </Stack>
+        <Stack>
+          <Button
+            sx={{
+              bgcolor: "primary.main",
+              color: "white",
+              borderRadius: 10,
+              fontSize: "12px",
+              "&:hover": {
+                bgcolor: "primary.dark",
+              },
+            }}
+          >
+            Tweet
+          </Button>
+        </Stack>
+      </Stack>
     </Box>
   );
 };
